4b9a3acb5c422d869677e08002541d3bfbd27563
Pheszm <160299594+RazelMae@users.noreply.github.com>
2024-03-19 11:17:18 +0800

    TimePicker Fix

--- LabLog/Panels/DownloadReportsPanel.Designer.cs
+++ LabLog/Panels/DownloadReportsPanel.Designer.cs
@@ -51,7 +51,7 @@
             // dateTimePicker2
             // 
             this.dateTimePicker2.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dateTimePicker2.CustomFormat = "dd/mm/yy";
+            this.dateTimePicker2.CustomFormat = "dd/MM/yy";
             this.dateTimePicker2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
             this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
             this.dateTimePicker2.Location = new System.Drawing.Point(668, 268);
@@ -63,7 +63,7 @@
             // dateTimePicker1
             // 
             this.dateTimePicker1.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dateTimePicker1.CustomFormat = "dd/mm/yy";
+            this.dateTimePicker1.CustomFormat = "dd/MM/yy";
             this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
             this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
             this.dateTimePicker1.Location = new System.Drawing.Point(491, 268);
